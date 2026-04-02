96adb6faa4ed86e9836f103f05f858dd44a05cfd
Fernando Furtado <carrilho.furtado@gmail.com>
2026-04-02 16:29:41 -0300

    ignore out directory

--- out/language-server/main.js
+++
@@ -1,7 +0,0 @@
-import { createDefaultCoreModule, createDefaultSharedCoreModule, inject } from 'langium';
-import { DroneSolarInspectionGeneratedModule, dslProjectGeneratedSharedModule } from '../generated/module.js';
-export function createDSLServices(context) {
-    const shared = inject(createDefaultSharedCoreModule(context), dslProjectGeneratedSharedModule);
-    const DSL = inject(createDefaultCoreModule({ shared }), DroneSolarInspectionGeneratedModule);
-    return { shared, DSL };
-}
--- out/language/dsl-module.js
+++
@@ -1,9 +0,0 @@
-import { createDefaultCoreModule, createDefaultSharedCoreModule, inject, EmptyFileSystem } from 'langium';
-import { DroneSolarInspectionGeneratedModule, dslProjectGeneratedSharedModule } from '../generated/module.js';
-export function createDSLServices(context = EmptyFileSystem) {
-    const shared = inject(createDefaultSharedCoreModule(context), dslProjectGeneratedSharedModule // <-- Aqui também
-    );
-    const DSL = inject(createDefaultCoreModule({ shared }), DroneSolarInspectionGeneratedModule);
-    shared.ServiceRegistry.register(DSL);
-    return { shared, DSL };
-}
